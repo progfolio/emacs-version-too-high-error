95e17a2aa895f1fb563460f30ef4eff34998eb8b
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2022-01-29 22:01:51 -0500

    initial commit

---
+++ emvth.el
@@ -0,0 +1,9 @@
+;;; emvth.el --- a test                              -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  
+
+;; Author: Nicholas Vollmer <progfolio@protonmail.com>
+;; Created: December 10, 2019
+;; Keywords: org, convenience
+;; Package-Requires: ((emacs "99.99"))
+;; Version: 0.0.0
